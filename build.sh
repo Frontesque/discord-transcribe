4b950295793bd662d344b77dd8993c9e0042ec89
Kenny <27463495+Frontesque@users.noreply.github.com>
2026-03-11 21:13:50 -0400

    remove transcript from build. oops

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf bin
 rm -rf node_modules
-zip -r -o "tjsb.zip" * -x "./package-lock.json" -x "./build.sh" -x "./.git/*" -x "./*.zip"
+zip -r -o "tjsb.zip" * -x "./package-lock.json" -x "./build.sh" -x "./.git/*" -x "./*.zip" -x "./transcript.txt"
