3a35ea96860a406ce16f246cfb272c6672078161
Kenny <27463495+Frontesque@users.noreply.github.com>
2026-03-11 20:38:47 -0400

    update splash

--- dependencies.sh
+++ dependencies.sh
@@ -7,10 +7,13 @@ case "$MODEL_TO_DOWNLOAD" in tiny|tiny.en|tiny-q5_1|tiny.en-q5_1|tiny-q8_0|base|
         echo "Valid model selected: $MODEL_TO_DOWNLOAD"
         ;;
     *)
-        echo "Invalid or no model specified. Defaulting to base.en"
-        MODEL_TO_DOWNLOAD="base.en"
+        echo "Invalid or no model specified. Please set the "SCRIPTY_MODEL" environment variable. Exiting..."
+        exit
         ;;
 esac
+
+echo -e "###########################################################################\n#                                                                         #\n#   STOP!                                                                 #\n#                                                                         #\n#   THIS EXECUTABLE IS PROPERTY OF DROP NOTIFIER L.L.C.                   #\n#                                                                         #\n#   IF YOU ARE NOT AUTHORIZED TO USE THIS EXECUTABLE, PLEASE QUIT NOW.    #\n#                                                                         #\n###########################################################################"
+sleep 5
 
 mkdir -p bin
 cd bin
